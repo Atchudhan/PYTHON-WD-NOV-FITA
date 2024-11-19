354fa47ad64e5fff7a8ff254552915dd87508cf0
Sitharthsithu <145182290+Sitharthsithu@users.noreply.github.com>
2024-11-19 20:06:43 +0530

    Add files via upload

---
+++ SITHARTH/def_function.py
@@ -0,0 +1,7 @@
+x=24
+def mental(name):
+    y="thiyagu"
+    print(x," hrs mental ",name )
+def loosu():
+    x=69
+    print(x ,"thiyagu with himself ")
---
+++ SITHARTH/import2.py
@@ -0,0 +1,5 @@
+import def_function
+def_function.mental("thiyagu")
+def_function.loosu()
+s=dir(def_function)
+print(s)
